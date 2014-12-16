70b302d222bed9fca75c1baaa3c0874744c3d111
Chris Prescott <cmprescott@gmail.com>
2014-12-16 00:14:15 -0500

    Adding example that fades a RGBW light blub zone in using bash script. Parameters are 1. zone 2. seconds between increasing brightness 3. color

---
+++ examples/color_fade_in.sh
@@ -0,0 +1,45 @@
+##########
+# About - color_fade_in.sh
+##########
+# This script slowly changes a zone of Limitless RGBW lightbulbs from the darkest setting to the brightest.
+# An example use case would be for using your lights to help you wake up in the morning.
+
+##########
+# Config
+##########
+# Please configure the location of led.sh in the script. The script also take 3 parameters (zone, sleep time, color)
+path_to_led_script="/home/cp/MiLight/limitlessled-bash/"
+
+##########
+# Input
+##########
+# Example usage: ./color_fade_in.sh 0 60 white
+zone=$1
+sleep_time=$2
+color=$3
+
+##########
+# Setup. Go to script loc, turn on, and set color.
+##########
+cd $path_to_led_script
+./led.sh c $zone on
+sleep 1
+./led.sh c $zone c $color
+sleep 1
+
+##########
+# Function changes brightness level and sleeps for configged amount of time
+##########
+function changeBrightAndSleep {
+    level=$1
+    ./led.sh c $zone b $level
+    sleep $sleep_time
+}
+
+##########
+# Iterate starting at lowest brightness all the way until highest brightness
+##########
+for i in `seq 0 18`;
+do
+    changeBrightAndSleep $i
+done    
